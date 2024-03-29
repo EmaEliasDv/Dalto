09a3554519c179f353d1d6ce097889198c8a2ef4
EmaEliasDv <emanuel.elias.davalos@gmail.com>
2024-03-29 17:12:11 -0300

    Repositorio de cursos

---
+++ main.js
@@ -0,0 +1,89 @@
+/*---------------------------------------*/
+/*--------------FUNCIONES---------------*/
+/*-------------------------------------*/
+
+//Declarar la función.
+
+/* function saludar() {
+  respuesta = prompt("Hola como estas?");
+  if (respuesta == "bien") {
+    alert("Que bueno, me alegro!");
+  } else {
+    alert("Okey, espero que tu día que mejore!");
+  }
+} */
+
+/* Llamar o invocar la función, solo con llamarla evitamos escribir 
+nuevamente todo el bloque de código */
+
+// saludar();
+// saludar();
+
+/* No es recomendable crear un función que tenga muchas tareas, sino mas
+bien que tenga una tarea especifica */
+
+/*--------------RETURN---------------*/
+
+/* Se declara una función la cual se guarda dentro de la variable "saludo",
+se llama dentro del console.log() y devuelve lo que esté en el return*/
+
+/*function saludar () {
+    alert("hola")
+    return ("Dentro de este 'Return' está el valor de 'Saludo'.")
+}
+
+let saludo = saludar();
+
+console.log(saludo); */
+
+/*-------------PARAMETROS--------------*/
+
+// Los Parametros son variables que se declaran y crean dentro de una función.
+
+// Ejemplo
+
+/*let num1 = 15;
+let num2 = 15;
+
+function sumar() {
+    return(num1 + num2) 
+}
+
+console.log(sumar()); */
+
+// Ejemplo
+
+/*function sumar(a, b) {
+  return a + b;
+}
+
+console.log(sumar(15, 15)); */
+
+// Ejemplo
+
+/*function sumar(num1, num2) {
+    let res = num1 + num2;
+
+    return res;
+}
+
+let resultado = sumar(15, 45);
+
+console.log(`El resultado de la suma es: ${resultado}`); */
+
+// Ejemplo
+
+/*function saludar(nombre) {
+  let frase = `Hola ${nombre}! ¿Como estas?`;
+  console.log(frase);
+}
+
+saludar("Elias") */
+
+/*-------------FUNCIÓN FLECHA--------------*/
+
+/*const saludar = (nombre) => {
+  console.log(`Hola ${nombre}! ¿Cómo estás?`);
+};
+
+saludar("Emanuel"); */
